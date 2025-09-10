f5b6cb953686ea6923132a19f0e56f191e2f9aa2
BOOPATHI-RAJAMANI <boopathi.aids@gmail.com>
2025-09-10 12:13:05 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -149,12 +149,14 @@ const Hero = () => {
                       <ArrowRight className="ml-2 h-5 w-5" />
                     </Link>
                   </Button>
-                  <a href="/About'>
+                  
                   <Button variant="outline" size="lg" className="btn-outline-premium">
                     <Play className="mr-2 h-5 w-5" />
-                    Watch Demo
+                     <Link to="/contact">
+                    Watch Deme
+                 </Link>
                   </Button>
-                </a>
+              
                 </div>
 
                 {/* Stats */}
