58b5a4610757c06785d8353eb685812542433e66
BOOPATHI-RAJAMANI <boopathi.aids@gmail.com>
2025-09-10 12:32:11 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -165,7 +165,7 @@ const About = () => {
             <iframe
               width="100%"
               height="400"
-              src="https://youtu.be/xzLfm7e3eCE"
+              src="https://youtu.be/ADVwdjEdHa8"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
